45f0089f8b498fcfbf42d096a4f65074f7e899e7
Dangkien1508 <119397082+Dangkien1508@users.noreply.github.com>
2022-12-30 12:58:42 +0700

    Create Old wallet run.sh

---
+++ Old wallet run.sh
@@ -0,0 +1,123 @@
+echo -e "\n==========INSTALLING DEPENDENCIES==========\n"
+sleep 2
+sudo apt-get update
+sudo apt-get install -y make git wget curl screen
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL repo.chainflip.io/keys/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/chainflip.gpg
+
+
+echo -e "\n==========CHECKING CHAINFLIP GPG KEY==========\n"
+sleep 2
+
+gpg --show-keys /etc/apt/keyrings/chainflip.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/chainflip.gpg] https://repo.chainflip.io/perseverance/ focal main" >> /etc/apt/sources.list.d/chainflip.list
+
+
+echo -e "\n==========INSTALLING CHAINFLIP PACKAGE==========\n"
+sleep 2
+
+sudo apt-get update
+sudo apt-get install -y chainflip-cli chainflip-node chainflip-engine
+
+echo -e "\n==========SETUP ETHEREUM KEY==========\n"
+sleep 2
+
+if [[ -d /etc/chainflip/keys ]]; then
+    sudo rm -rf /etc/chainflip/keys
+fi
+
+sudo mkdir /etc/chainflip/keys
+
+read -p "Enter ethereum private keys (without 0x): " ETH_PRIVATE_KEY
+echo -n "$ETH_PRIVATE_KEY" >> /etc/chainflip/keys/ethereum_key_file
+
+echo -e "\n==========SETUP SIGNIN KEY==========\n"
+sleep 2
+
+if [[ -s sign_key.txt ]]; then
+    rm sign_key.txt
+fi
+
+read -p "Enter Secret phrase: " Secret_phrase
+echo -n "Secret_phrase" >> sign_key.txt
+sleep 2
+
+read -p "Enter Network ID: "  Network_ID
+echo -n "Network_ID" >> sign_key.txt
+sleep 2
+
+read -p "Enter Secret seed: "  Secret_seed
+echo -n "Secret_seed" >> sign_key.txt
+sleep 2
+
+read -p "Enter Public key (hex): "  Public_key
+echo -n "Public_key (hex)" >> sign_key.txt
+sleep 2
+
+read -p "Enter account ID: "  Account_ID
+echo -n "Account_ID" >> sign_key.txt
+sleep 2
+
+read -p "Enter Public key (SS58): "  Public_key
+echo -n "Public_key_(SS58)" >> sign_key.txt
+sleep 2
+
+read -p "Enter SS58 Address: "  SS58_Address
+echo -n "SS58_Address" >> sign_key.txt
+sleep 2
+chainflip-node key generate >> sign_key.txt
+
+
+SIGNING_KEY=$(cat sign_key.txt)
+echo -e "$SIGNING_KEY\n"
+
+SECRET_SEED=$(grep "Secret seed" sign_key.txt)
+echo -n "${SECRET_SEED:23}" >> /etc/chainflip/keys/signing_key_file
+
+echo -e "==========GENERATING NODE KEY==========\n"
+sleep 2
+
+sudo chainflip-node key generate-node-key --file /etc/chainflip/keys/node_key_file
+
+echo -e "\n==========GENERATING Default.toml==========\n"
+sleep 2
+
+if [[ -d /etc/chainflip/config ]]; then
+    rm -rf /etc/chainflip/config
+fi
+
+sudo mkdir -p /etc/chainflip/config
+IP_ADDRESS=$(curl -sw "\n" ifconfig.me)
+
+read -p "Enter WSS Node endpoint: " WSS_NODE_ENDPOINT
+read -p "Enter HTTP Node endpoint: " HTTP_NODE_ENDPOINT
+
+echo '# Default configurations for the CFE
+[node_p2p]
+node_key_file = "/etc/chainflip/keys/node_key_file"' >> /etc/chainflip/config/Default.toml
+echo 'ip_address = '\"${IP_ADDRESS}\" >> /etc/chainflip/config/Default.toml
+echo 'port = "8078"
+
+[state_chain]
+ws_endpoint = "ws://127.0.0.1:9944"
+signing_key_file = "/etc/chainflip/keys/signing_key_file"
+
+[eth]
+# Ethereum RPC endpoints (websocket and http for redundancy).' >> /etc/chainflip/config/Default.toml
+echo 'ws_node_endpoint = '\"${WSS_NODE_ENDPOINT}\" >> /etc/chainflip/config/Default.toml
+echo 'http_node_endpoint = '\"${HTTP_NODE_ENDPOINT}\" >> /etc/chainflip/config/Default.toml
+
+echo '
+# Ethereum private key file path. This file should contain a hex-encoded private key.
+private_key_file = "/etc/chainflip/keys/ethereum_key_file"
+
+[signing]
+db_file = "/etc/chainflip/data.db"' >> /etc/chainflip/config/Default.toml
+
+sleep 2
+
+echo -e "\n==========SETUP FINISH==========\n"
+echo -e "To start node run \`sudo systemctl start chainflip-node\`"
+echo -e "To check node status run \`sudo systemctl status chainflip-node\`"
